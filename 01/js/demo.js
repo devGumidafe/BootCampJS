eec63f74b3fc61c48000d1e70941e596c56c9599
Gumidafe <devgumidafe@gmail.com>
2021-06-09 16:31:06 +0100

    4.- Add imagen

--- 01/js/demo.js
+++ 01/js/demo.js
@@ -1,5 +1,11 @@
 let inputName = document.getElementById('name');
 let inputLastName = document.getElementById('lastName');
+let imgAvatar = document.getElementById('avatar');
 
-inputName.value= 'Gumidafe';
+inputName.value = 'Gumidafe';
 inputLastName.value = 'Socorro Delgado';
+imgAvatar.src = '../img/avatar.png';
+
+function showInput() {
+    console.log(lastName.value);
+}
