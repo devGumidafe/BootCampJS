cb60ddac88ead7c206ca12a4b9eea13f57998689
Gumidafe <devgumidafe@gmail.com>
2021-06-10 12:50:38 +0100

    3.- Mostrar puntuación del usuario

--- 02/js/app.js
+++ 02/js/app.js
@@ -1,5 +1,5 @@
 
-// Objecto Hotel
+// Objeto Hotel
 const hotel = {
     name: 'Baobab',
     location: 'Gran Canaria',
@@ -12,10 +12,15 @@ let nameHotel = document.getElementById('nameHotel');
 let locationHotel = document.getElementById('locationHotel');
 let description = document.getElementById('description');
 let imgHotel = document.getElementById('imgHotel');
-
+let rating = document.getElementById('rating');
 
 // Asignamos valores
 nameHotel.innerHTML = 'Hotel ' + hotel.name;
 locationHotel.innerHTML = 'Ubicado en ' + hotel.location;
 description.innerHTML = hotel.description;
 imgHotel.src = hotel.img;
+
+let respRating = prompt ('Elige una puntuacion del 1 al 5');
+rating.innerHTML = respRating + ' estrellas';
+
+
