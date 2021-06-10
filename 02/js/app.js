2da1cda99115dcb0f8a0d80ce4f9bbfed112b9a2
Gumidafe <devgumidafe@gmail.com>
2021-06-10 13:21:43 +0100

    Opcional 1.- Creamos 3 hoteles y permitmos al usuario hacer reseña sobre uno de ellos

--- 02/js/app.js
+++ 02/js/app.js
@@ -1,13 +1,30 @@
 
-// Objeto Hotel
-const hotel = {
-    name: 'Baobab',
-    location: 'Gran Canaria',
-    description: 'Vivir unas vacaciones de lujo y diversión con un toque exótico',
-    img: '../img/baobab.png'
+// Objeto Hoteles
+const hotels =
+{
+    Baobab: {
+        name: 'Baobab',
+        location: 'Gran Canaria',
+        description: 'Vivir unas vacaciones de lujo y diversión con un toque exótico',
+        img: '../img/baobab.png'
+    },
+    Costa: {
+        name: 'Costa Meloneras',
+        location: 'Gran Canaria',
+        description: 'Lugar mágico situado en Meloneras, la zona turística más exclusiva de Gran Canaria.',
+        img: '../img/costa.png'
+    },
+    Villa: {
+        name: 'Villa del Conde',
+        location: 'Gran Canaria',
+        description: ' Hotel único en toda Gran Canaria, no solo por los servicios que ofrece sino también por su singular diseño.',
+        img: '../img/villa.png'
+    }
 }
 
 // Creamos variables
+let selectedHotel = prompt('Elige el hotel al que quiere realizar la reseña: Baobab, Costa y Villa');
+
 let nameHotel = document.getElementById('nameHotel');
 let locationHotel = document.getElementById('locationHotel');
 let description = document.getElementById('description');
@@ -16,14 +33,13 @@ let rating = document.getElementById('rating');
 let isAnonymous = document.getElementById('anonymous');
 
 // Asignamos valores
-nameHotel.innerHTML = 'Hotel ' + hotel.name;
-locationHotel.innerHTML = 'Ubicado en ' + hotel.location;
-description.innerHTML = hotel.description;
-imgHotel.src = hotel.img;
+nameHotel.innerHTML = 'Hotel ' + hotels[selectedHotel].name;
+locationHotel.innerHTML = 'Ubicado en ' + hotels[selectedHotel].location;
+description.innerHTML = hotels[selectedHotel].description;
+imgHotel.src = hotels[selectedHotel].img;
 
-let respRating = prompt ('Elige una puntuacion del 1 al 5');
+let respRating = prompt('Elige una puntuacion del 1 al 5');
 rating.innerHTML = respRating + ' estrellas';
-
 
 let respAnonymous = confirm('¿Quiere que la reseña sea anónima?');
 isAnonymous.checked = respAnonymous;
