29ebc8df79ae4da427a0b7b6a815769faad9bad3
Gumidafe <devgumidafe@gmail.com>
2021-06-11 15:23:59 +0100

    Calculo final de un producto

--- 03/calcularPrecio.js
+++ 03/calcularPrecio.js
@@ -1,44 +1,20 @@
 
 //Impuestos dependiendo del tipo de producto
-const vatDefault = 0.21; //
-const vatFood = 0.10;
-const vatBook = 0.04;
+const vatDefault = 0.21; // iva por defecto
+const vatFood = 0.10; //iva alimentación
+const vatBook = 0.04; // iva libros
 
 //Productos
-const product_1 = { count: 3, price: 12.55, type: "electronics" };
+const product_1 = { count: 7, price: 12.55, type: "electronics" };
 const product_2 = { count: 10, price: 2.99, type: "food" };
 const product_3 = { count: 5, price: 19.99, type: "book" };
 
-//Función que calcula el total del producto sumandole su correspondiente iva
+//Función que calcula el subtotal de un producto
 function getTotal(product) {
-
-    if (product.count < 0) return 0;
-
-    const subTotal = product.count * product.price;
-    vat = 0;
-
-    switch (product.type) {
-        case "food":
-            vat = subTotal * vatFood;
-            break;
-
-        case "book":
-            vat = subTotal * vatBook;
-            break;
-
-        default:
-            vat = subTotal * vatDefault
-            break;
-    }
-
-    return subTotal + vat;
+    return product.count > 0 ? product.count * product.price : 0;
 }
 
-const totalProduct = getTotal(product_3);
-console.log('Total producto:', totalProduct, '€');
-
-
-//Función que calcula el iva de un producto
+//Función que calcula el iva por unidad de un producto
 function getVat(product) {
     if (product.price <= 0) return 0;
 
@@ -54,5 +30,23 @@ function getVat(product) {
     }
 }
 
-const vatProduct = getVat(product_1)
-console.log('IVA del producto:',vatProduct,'€')
+//Función que calcula el total del iva de un producto
+function getTotalVat(product) {
+    return product.count > 0 ? product.count * getVat(product) : 0;
+}
+
+//Función que imprime el total e iva de un producto
+function printProductPrice(product) {
+    const subTotal = getTotal(product);
+    const vat = getTotalVat(product);
+    const total = subTotal + vat;
+
+    console.log(`Subtotal: ${subTotal} €`);
+    console.log(`IVA: ${vat} €`);
+    console.log(`Total: ${total} €`);
+}
+
+printProductPrice(product_1);
+
+
+
