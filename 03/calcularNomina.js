f98712875400a0dad615e724da5d685be6e09503
Gumidafe <devgumidafe@gmail.com>
2021-06-11 18:26:27 +0100

    Calculo salario neto

--- 03/calcularNomina.js
+++ 03/calcularNomina.js
@@ -1,11 +1,17 @@
 // Empleados
-
 const empleado_1 = {
     bruto: 24000,
-    hijos: 0,
+    hijos: 2,
     pagas: 12
 }
 
+const empleado_2 = {
+    bruto: 1800,
+    hijos: 0,
+    pagas: 14
+}
+
+// Función que calcula la retención según salario y numero de hijos
 function getRetencion(empleado) {
     const bruto = empleado.bruto;
     const retencionPorHijos = getRetencionPorHijos(empleado);
@@ -27,10 +33,12 @@ function getRetencion(empleado) {
     }
 }
 
+
 function getRetencionPorHijos(empleado) {
     return empleado.hijos > 0 ? 0.02 : 0;
 }
 
+// Función que imprime la retención, salario anual y mensual
 function getPrintSalarioNeto(empleado) {
     const bruto = empleado.bruto;
     const retencion = getRetencion(empleado);
