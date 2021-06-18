1bdec4b278d66f3800185bffd4f183b87dddc24a
Gumidafe <devgumidafe@gmail.com>
2021-06-18 12:30:35 +0100

    Add funciones para crear elementos html y funciones para cacular el precio

--- 06/js/app.js
+++ 06/js/app.js
@@ -62,3 +62,81 @@ const products = [
         units: 0,
     },
 ];
+
+// Elementos del DOM
+const productsContainer = document.getElementById('products-container');
+const btnCalculate = document.getElementById('btnCalculate');
+const subtotal = document.getElementById('subtotal');
+const iva = document.getElementById('iva');
+const total = document.getElementById('total');
+
+
+// FUNCIONES PARA CREAR LOS ELEMENTOS HTML
+const createINPUT = (product) => {
+    let input = document.createElement("input");
+    input.setAttribute("class", "product-unit");
+    input.setAttribute("type", "number");
+    input.setAttribute("value", product.units);
+    input.setAttribute("min", 0);
+    input.setAttribute("max", product.stock)
+    input.addEventListener("change", event => product.units = Number(event.target.value));
+
+    return input;
+}
+
+const createLI = (product) => {
+    let input = createINPUT(product);
+
+    let li = document.createElement("li");
+    li.setAttribute("class", "product");
+    li.textContent = `${product.description} - ${product.price}€/ud.`;
+    li.appendChild(input);
+
+    return li;
+}
+
+const createOL = (productList) => {
+    const ol = document.createElement("ol");
+    ol.setAttribute("class", "list-product");
+
+    for (product of productList) {
+        ol.appendChild(createLI(product))
+    }
+
+    return ol;
+}
+
+const printList = (productList) => {
+    productsContainer.appendChild(createOL(productList))
+}
+
+
+
+// FUNCIONES PARA CALCULAR EL PRECIO
+const getIVA = () => {
+    let IVA = 0;
+
+}
+
+const productCost = (product) => {
+    let productCost = product.price * product.units;
+    return productCost * (1 + product.tax / 100);
+}
+
+const totalCost = (productList) => {
+    let totalCost = 0;
+
+    for (product of productList) {
+        totalCost += productCost(product);
+    }
+    return totalCost;
+}
+
+
+// USAMOS LAS FUNCIONES
+printList(products);
+
+btnCalculate.addEventListener('click', () => {
+    let t = totalCost(products);
+    total.textContent = t.toFixed(2)
+})
